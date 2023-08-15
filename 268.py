b6fffc15c85098f25639e46c55c5c1df557f0a60
biologycoder <142151479+biologycoder@users.noreply.github.com>
2023-08-15 19:22:42 +0800

    Update 268.py

--- 268.py
+++ 268.py
@@ -1,5 +1,5 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
+    def missingNumber(self, nums):
         for i in range(len(nums)+1):
             if i not in nums:
                 return i
